dd86fbcde09327562774d30bc45ad8d16161b1ef
nerkn <erkintek@gmail.com>
2025-10-23 16:22:17 +0300

    Add initial project files

---
+++ scripts/genJson.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import mysql from "mysql2/promise";
+
+const conn = await mysql.createConnection(process.env.DB_URL);
+
+const [tags] = await conn.query("SELECT * FROM news_tags");
+const [news] = await conn.query("SELECT * FROM news_article order by id desc limit 500");
+
+const dist = path.resolve("dist");
+fs.mkdirSync(dist, { recursive: true });
+fs.copyFileSync("index.html", path.join(dist, "index.html"));
+fs.writeFileSync(path.join(dist, "tags.json"), JSON.stringify(tags));
+fs.writeFileSync(path.join(dist, "news.json"), JSON.stringify(news));
+
+await conn.end();
